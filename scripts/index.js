f61ad102c1552cf3cb21473a01f826ab9bd89c87
goldlilya1612 <goldlilya1612@yandex.com>
2020-11-29 16:13:26 +0300

    project-4_5

--- scripts/index.js
+++ scripts/index.js
@@ -8,28 +8,29 @@ let profileTextNode = document.querySelector('.profile__text');
 
 let popupContainerNode = document.querySelector('.popup__container');
 let popupButtonNode = document.querySelector('.popup__save-button');
-let fieldNameNode = document.querySelector('.popup__field-name');
-let fieldAboutNode = document.querySelector('.popup__field-about');
+let fieldNameNode = document.querySelector('.popup__field_name');
+let fieldAboutNode = document.querySelector('.popup__field_about');
 
 
 function handleEditButtonClick() {
-    popupNode.classList.add('popup__opened');
+    popupNode.classList.add('popup_opened');
     fieldNameNode.value = profileNameNode.textContent;
     fieldAboutNode.value = profileTextNode.textContent;
 }
 
 function handlePopupCloseButtonClick() {
-    popupNode.classList.remove('popup__opened');
+    popupNode.classList.remove('popup_opened');
 }
 
 function handlePopupContainerSubmit() {
     event.preventDefault();
     profileNameNode.textContent = fieldNameNode.value;
     profileTextNode.textContent = fieldAboutNode.value;
-    popupNode.classList.remove('popup__opened');
+    handlePopupCloseButtonClick();
 }
+
 
 
 editButtonNode.addEventListener('click', handleEditButtonClick);
 popupCloseButtonNode.addEventListener('click', handlePopupCloseButtonClick);
-popupContainerNode.addEventListener('submit', handlePopupContainerSubmit)
+popupContainerNode.addEventListener('submit', handlePopupContainerSubmit);
